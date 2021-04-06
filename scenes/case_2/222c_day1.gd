80133a358bcfa9f05a5834e3121850757a516323
SuperTemich2005 <SuperTemich2005@gmail.com>
2021-04-07 00:25:55 +0300

    asd

--- scenes/case_2/222c_day1.gd
+++ scenes/case_2/222c_day1.gd
@@ -2,33 +2,44 @@ extends Node2D
 var Dialogue
 func _ready():
 	Dialogue = [
-		"0",
-		"'Jak kit mamy mam...'",
-		"1/2: Kit",
-		"Ty mamy mam? G",
-		"SPLIT Tak Nie 7 4",
-		"Jak ne mam?",
-		"Ty tata mam?",
-		"JUMP 1",
-		"Faine",
-		"A sestry mam?",
-		"JUMP 11",
-		"asdasdasd",
-		"11/12: эту строку",
-		"placeholder",
-		"белый W",
-		"зеленый G",
-		"оранжевый R",
-		"желтый Y",
-		"телепатическирозовый P",
-		"(w glowie mysli mam) B",
+		"",
+		"5 октября. 12:00. Номер 222с G",
+		"Тёмыч: Не, нихуя, мы таки все еще тестим W",
+		"Тесты вечны.",
+		"",
+		"MAIN",
+		"1",
+		"2",
+		"3",
+		"Зеркало, хуле", # 5, зеркало
+		"SPLIT Расследовать ГлавМеню 10 12",
+		"Это стоит осмотреть",
+		"EXAM",
+		"Нафиг надо",
 		"MAIN",
 		"",
-		"JUMP 1",
-		"",
 	]
-
+# 1) цвет, MOV-слова срабатывают мгновенно
+# 2) SPLIT, JUMP срабатывают раньше на 1 реплику
+# 3) чтобы прыгнуть на N-ную строку, надо из номера строки в редакторе
+# вычесть 6
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_poi_1_pressed():
+	if $investigation_screen.State == "Examine":
+		$investigation_screen.State = "Dialogue"
+		$investigation_screen.Cur = int($poi_1.text)
+		$investigation_screen/show_text.text = Dialogue[$investigation_screen.Cur]
+		$investigation_screen.Checked[1] = true
+
+
+func _on_poi_1_mouse_entered():
+	if $investigation_screen.State == "Examine":
+		if $investigation_screen.Checked[1] == false:
+			$investigation_screen/crosshair.animation = "wut"
+		else:
+			$investigation_screen/crosshair.animation = "gotchu"
--- scenes/investigation.gd
+++ scenes/investigation.gd
@@ -7,8 +7,19 @@ extends Node2D
 
 var Cur
 var State : String
+var CourtRecord : Array
+var Checked : Array
+var save_file
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	CourtRecord.resize(12)
+	Checked.resize(10)
+	for i in range(0,10):
+		Checked[i] = false
+	save_file = File.new()
+	save_file.open("C:/Games/ddkg2.save",File.READ)
+	for i in range(1,save_file.get_as_text().split(" ").size()):
+		CourtRecord.append(save_file.get_as_text().split(" ")[i])
 	Cur = 0
 	State = "Dialogue" # Main Dialogue Examine ChatSelect Present Move
 
@@ -50,15 +61,17 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		$crosshair.position = event.position
 func _on_next_button_pressed():
-	print(get_parent().Dialogue[Cur+1].split(" ")[0])
 	Cur+=1
+	print(get_parent().Dialogue[Cur+1].split(" ")[0])
 	$show_text.text = get_parent().Dialogue[Cur]
-	if get_parent().Dialogue[Cur+1].split(" ")[0] == "JUMP":
+	if get_parent().Dialogue[Cur].split(" ")[0] == "JUMP":
 		Cur = int(get_parent().Dialogue[Cur+1].split(" ")[1])
 		#$show_text.text = get_parent().Dialogue[Cur]
 		print("jumping to whatever")
-	if get_parent().Dialogue[Cur+1].split(" ")[0] == "MAIN":
+	if get_parent().Dialogue[Cur].split(" ")[0] == "MAIN":
 		State = "Main"
+	if get_parent().Dialogue[Cur].split(" ")[0] == "EXAM":
+		State = "Examine"
 	if get_parent().Dialogue[Cur+1].split(" ")[0] == "SPLIT":
 		$choice_first.disabled = false
 		$choice_second.disabled = false
@@ -115,3 +128,6 @@ func _on_back_button_pressed():
 
 func _on_button_investigate_pressed():
 	State = "Examine"
+
+
+
