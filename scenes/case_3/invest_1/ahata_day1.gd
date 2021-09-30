9ef24d189718bddb63ec205c8042e6f45a44b895
SuperTemich2005 <39464061+SuperTemich2005@users.noreply.github.com>
2021-10-01 00:54:55 +0300

    111

--- scenes/case_3/invest_1/ahata_day1.gd
+++ scenes/case_3/invest_1/ahata_day1.gd
@@ -11,36 +11,16 @@ var loc_file = ConfigFile.new()
 func _ready():
 	Dialogue = [ # диалоги. 
 		"",
-		"6 октября. 12:00. Перон Львовского вокзала.|G",
-		"Егорыч: Наконец то мы приехали!|W|---|START peaceful_day|---",
-		"Ууууух...",
-		"После этих часов сидения, потянуться не помешает...",
-		"Тёмыч: УУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУХ!|W|character_temich default|---|REACT damage",
-		"Егорыч: Это было... весьма показательно.",
-		"Е. Анатольевна: Ждем, пока остальные выйдут...|W|character_lentoln default|---|---",
-		"Егорыч: Угу.",
-		"Лентольна, куда мы будем сегодня ездить?",
-		"Е. Анатольевна: Ну, во Львове есть очень интересная достопримечательность.|W|character_lentoln hmm|---|---",
-		"Замок Текуцких|R|character_lentoln kek|---|REACT surprise",
-		"Тёмыч: Ну и ну, Лентольна. Да вы у нас 'голубых кровей'?|W|character_temich kekk|---|---",
-		"Егорыч: (Если бы кровь Лентольны была основана не на железе, а на кобальте, то да, именно 'голубых кровей')|B",
-		"Е. Анатольевна: Но сначала, мы планируем заехать в 'Пузату Хату'. Мы там как раз таки на 12:10 забронировали определенное кол-во столов.|W|character_lentoln hmm|---|---",
-		"Все хотят есть.",
-		"Тёмыч: Солидарен с теми, кто хочет есть.|W|character_temich wonder|---|---",
-		"Е. Анатольевна: О, Артём, а где твой плащ?|W|character_lentoln default|---|---",
-		"Тёмыч: Это была кофта, а не плащ...|W|character_temich kekk|---|---",
-		"По этому я и переоделся, собственно.|W|character_temich kekk|---|---",
-		"Е. Анатольевна: Во, наконец-то последние вышли.|W|character_lentoln default|---|---",
-		"Егорыч: (Странно, я думал, что нас меньше.)|B",
-		"OUT res://scenes/case_3/invest_1/ahata_day1.tscn",
+		"6 октября. 12:50. 'Пузатий пан' 1-й этаж.|G",
+		"Тёмыч: ",
 		"",
 	]
 # СУКА БЛЯТЬ НЕ ТРОГАЙ ЭТО ГАНДОН НЕДОШТОПАННЫЙ АААААААААААААААААААААААА!!!!!!!!!!!!!!!!!!!!!!!
 	loc_file.load("C:/Games/ddkg2.save")
 	Chats = [
-		"Как экскурсия? 84",
-		"Соня 97",
-		"'Некое дело' 133",
+		"",
+		"",
+		"",
 		"",
 	]
 	Moves = [
@@ -51,18 +31,16 @@ func _ready():
 	]
 	Shows = [
 		"badge 287",
-		"badge 287",
-		"badge 287",
 	]
-	for i in range(1,4):
+	for i in range(1,5):
 		if Chats[i-1] != "":
 			get_node("chat_"+str(i)).text = Chats[i-1].left(Chats[i-1].length()-3)
-	for i in range(1,4):
+	for i in range(1,5):
 		if Moves[i-1] != "":
 			get_node("move_"+str(i)).text = Moves[i-1].left(Moves[i-1].length()-Moves[i-1].split(" ")[-1].length())
 	print(Chats[0].split(" ")[-1])
 	$investigation_screen/frame_record/record_theme.color = Color(0.5,1,0.5)
-	EvCount = 4
+
 
 func _on_chat_1_pressed():
 	if $investigation_screen.State == "Chat" and Chats[0] != "":
